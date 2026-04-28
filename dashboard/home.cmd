4049220a894db5d0d72c46bb67c4db03ac63d243
promotoraqueiroz@gmail.com <promotoraqueiroz@gmail.com>
2026-04-27 21:56:17 -0300

    aula nova

---
+++ dashboard/home.cmd
@@ -0,0 +1,11 @@
+
+*temos 3 paginas dentro da pasta dashboard:
+dashboard.html - página base com informações no front
+dashboard.css - aqui coloque toda a estilisação da página dashboard.html
+depois
+criei a pagina onde coloquei os códigos que colocou os cards de produtos na página inicial do html, chamada dashboard.js
+usamos o java para fazer isso
+
+obs: Loiane Groner, professora de cursos e escritora.
+*a pasta home é uma página de login, ainda não vamos trabalhar com ela agora
+
